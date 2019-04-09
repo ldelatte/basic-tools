a3782412b1c32d4798b3b857fe131a8335cc84e5
ldelatte <38010476+ldelatte@users.noreply.github.com>
2019-04-09 14:51:10 +0200

    Update dklog.sh

--- dklog.sh
+++ dklog.sh
@@ -1,24 +1,34 @@
-#!/bin/ksh
+#!/bin/bash
 # creation: Laurent Delatte 03/2019
-# maj: ...
+# modifications: ...
 # objet: log multi conteneurs
-# arg1: chaine
-# arg2: [ anciennete ]
-#
-[ "$1" ] || { echo filtre obligatoire; exit 1; }
-pid=""
-docker container ls --format "{{.Names}}" --filter name=$1 |
-if [ "$2" = "" ] ;then
+
+while getopts "f:m:h:?" opt; do
+  case $opt in
+    f)
+      FILTRE=$OPTARG ;;
+    m)
+      ANC=${OPTARG}m ;;
+    h)
+      ANC=${OPTARG}h ;;
+    *)
+      echo "Usage: dklog.sh -f <nom partiel des conteneurs> [ -m <nb de minutes> | -h <nb d'heures> (defaut: 5m avec suivi) ]" >&2
+      exit 1 ;;
+  esac
+done
+
+[ "$FILTRE" ] || { $0 -?; exit 1; }
+
+docker container ls --format "{{.Names}}" --filter name=$FILTRE |
+if [ "$ANC" ] ;then
+  while read c;do
+    docker container logs --since $ANC $c
+  done
+else
   while read c;do
     docker container logs --since 5m -f $c &
     pid="$pid $!"
   done
-else
-  while read c;do
-    docker container logs --since $2 $c
-  done
-fi
-if [ "$pid" ] ;then
-  read fin
+  sleep 60
   kill -15 $pid
 fi
