b64dc1f634ebb5e8408d170c6958d4c7b6e61aef
ldelatte <38010476+ldelatte@users.noreply.github.com>
2019-05-15 17:08:47 +0200

    Update AppCheck.sh

--- AppCheck.sh
+++ AppCheck.sh
@@ -13,12 +13,12 @@ ETC=.
 [ "$LOGTAG" ] || LOGTAG=AppCheck
 NbChn=2
 distrib=0
-config=prd.AppCheckIn
+config=prd-AppCheckIn
 while getopts "e:dm?" opt; do
   case $opt in
   e) case $OPTARG in
-     "debug") config=debug.AppCheckIn ;;
-     "dev"|"int"|"rct"|"prd") config=$OPTARG.AppCheckIn ;;
+     "debug") config=debug-AppCheckIn ;;
+     "dev"|"int"|"rct"|"prd") config=$OPTARG-AppCheckIn ;;
      *) $0 -?
         exit 1 ;;
      esac ;;
@@ -49,7 +49,7 @@ while read h l a c ;do
   else
     [ "$h" == "localhost" ] && echo r="\`$c\`" || echo r="\`ssh $h $c\`"
     [ "$h" == "localhost" ] && sortie="logger -s -t $LOGTAG" || sortie=echo
-    echo "[ \$r == $a ] || $sortie \"ERROR: Nombre d elements $l incorrect sur $h (\$r/$a)\""
+    echo "[ \$r == $a ] || $sortie \"ERROR: Nombre d elements $(echo $config|sed 's/-.*//')-$l incorrect sur $h (\$r/$a)\""
   fi
 done >/tmp/AppCheck.tmp
 sed -i s/\$NbChn/$NbChn/g /tmp/AppCheck.tmp
@@ -62,11 +62,12 @@ else
     cat <<EOF >/tmp/AppCheck.txt
 From: $SMTPFROM
 To: $( echo $SMTPDEST |sed -e 's/--mail-rcpt \([^ ]*\) /\1,/g' -e 's/--mail-rcpt \([^ ]*\)$/\1/' )
-Subject: [Supervision] Alarme du service interne de supervision
+Subject: $SMTPOBJECT
 Content-Type: text/html; charset="utf8"
-<html><body>
+<html><body><pre>
 EOF
     cat /tmp/AppCheck.log >>/tmp/AppCheck.txt
-    curl -s --url smtp://$SMTPHOST --user "$(cat $ETC/AppCheck.user)" --mail-from "$SMTPFROM" $SMTPDEST --upload-file /tmp/AppCheck.txt
+    echo "</pre></body></html>" >>/tmp/AppCheck.txt
+    curl -m3 -s --url smtp://$SMTPHOST --user "$(cat $ETC/AppCheck.user)" --mail-from "$SMTPFROM" $SMTPDEST --upload-file /tmp/AppCheck.txt
   fi
 fi
